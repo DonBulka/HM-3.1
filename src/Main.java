66b47609848e924641fb32b97c4c7cdbb0aec454
DonBulka <donbulka@yandex.ru>
2022-08-10 19:59:54 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 3000;
+        int bonus = ticket / 20;
+
+        System.out.println("Бонусных рублей начислено: " + bonus);
+
+
+    }
+}
